d751884d6df804a8965e093ad2a048566bc40cf9
Noah Swartz <swartzcr@gmail.com>
2016-10-03 13:15:02 -0400

    cookie

--- static/mystery.js
+++ static/mystery.js
@@ -1,5 +1,8 @@
 var img = document.getElementById("img");
 var node = document.createElement("IMG");
-node.src = "https://danielle.soy/spacer.gif";
-var fnx = function() {document.getElementById("division").appendChild(node);};
+node.src = "https://alinkisnotenabled.com/spacer.gif";
+var fnx = function( event ) {
+    event.preventDefault();
+    document.getElementById("division").appendChild(node);
+    window.location = "https://danielle.soy/puzzle2";};
 img.addEventListener("click", fnx);
