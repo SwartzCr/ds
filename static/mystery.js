9d64d3726836d3b647b792b6846ca3e79cbde1f7
Noah Swartz <swartzcr@gmail.com>
2016-10-03 11:47:43 -0400

    add remote call

--- static/mystery.js
+++ static/mystery.js
@@ -1,3 +1,5 @@
-var x = document.getElementById("image");
-//x.onclick = function() { var img = document.createElement("LI"); img.appendChild(document.createTextNode("hi")); x.appendChild(img);};
-//x.onclick = function() { x.style.visibility = "hidden";}
+var img = document.getElementById("img");
+var node = document.createElement("IMG");
+node.src = "https://danielle.soy/spacer.gif";
+var fnx = function() {document.getElementById("division").appendChild(node);};
+img.addEventListener("click", fnx);
